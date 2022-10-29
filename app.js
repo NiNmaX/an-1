2b6d00b32aef901865fa2daa5b1065f4269edea1
Abdusamadova Nigina <nigina2007abdusamadova@gmail.com>
2022-10-29 18:36:13 +0500

    site

---
+++ app.js
@@ -0,0 +1,8 @@
+function upper() {
+  var text = document.getElementById("text1");
+  text.value = text.value.toUpperCase();
+}
+function lower() {
+  var text = document.getElementById("text2");
+  text.value = text.value.toLowerCase();
+}
